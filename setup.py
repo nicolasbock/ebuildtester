6f619a4927b2ea7a94556dd5d3752c2fe04acce6
Nicolas Bock <nicolasbock@gmail.com>
2018-02-06 10:10:39 -0700

    Add first test

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
     name="ebuildtester",
     version="0.1.0",
     packages=find_packages(),
+    test_suite="tests",
     entry_points={
         "console_scripts": ["ebuildtester = ebuildtester.main:main"]
     }
---
+++ tests/test_parse.py
@@ -0,0 +1,7 @@
+import unittest
+
+
+class TestParse(unittest.TestCase):
+
+    def test_atom(self):
+        self.assertTrue(True)
