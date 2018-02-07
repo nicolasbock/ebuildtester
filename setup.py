92df84885c58ebb344a62bfda2c2130bd7aee3f8
Nicolas Bock <nicolasbock@gmail.com>
2018-02-07 12:39:02 -0700

    Version bump to v0.1.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ebuildtester",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
     entry_points={
         "console_scripts": ["ebuildtester = ebuildtester.main:main"]
