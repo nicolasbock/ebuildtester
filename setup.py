0ccba8cf52d575ee91371ce6e054b4c5d9c75db6
Nicolas Bock <nicolasbock@gmail.com>
2018-02-07 17:39:36 -0700

    Version v0.1.2

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="ebuildtester",
-    version="0.1.1",
-    packages=find_packages(),
+    version="0.1.2",
+    packages=["ebuildtester"],
     entry_points={
         "console_scripts": ["ebuildtester = ebuildtester.main:main"]
     },
     author="Nicolas Bock",
     author_email="nicolasbock@gmail.com",
-    description="A dockerized approach to test a Gentoo package within a clean stage3.",
+    description="A dockerized approach to test a Gentoo package within a clean stage3",
     license="BSD",
     url="https://github.com/nicolasbock/gentoo-test-package",
     project_urls={
