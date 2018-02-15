b4cd0957bc9eefb9aa05460218ce26326ea44be6
Nicolas Bock <nicolasbock@gmail.com>
2018-02-15 14:39:19 -0700

    Integrate sphinx build into setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+from sphinx.setup_command import BuildDoc
 from setuptools import setup, find_packages
 
 setup(
@@ -10,6 +11,7 @@ setup(
     entry_points={
         "console_scripts": ["ebuildtester = ebuildtester.main:main"]
     },
+    cmdclass={'build_sphinx': BuildDoc},
     author="Nicolas Bock",
     author_email="nicolasbock@gmail.com",
     description="A dockerized approach to test a Gentoo package within a clean stage3",
