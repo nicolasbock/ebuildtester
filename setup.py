9a0669ed546ad5797028ca06109f1f34687aa406
Nicolas Bock <nicolasbock@gmail.com>
2018-06-07 17:32:05 +0200

    Consolidate setup files

--- setup.py
+++ setup.py
@@ -6,6 +6,12 @@ with open("README.rst", "r") as fh:
     long_description = fh.read()
 
 setup(
+    name="ebuildtester",
+    author="Nicolas Bock",
+    author_email="nicolasbock@gmail.com",
+    description="A container approach to test a Gentoo package within a clean stage3",
+    license="BSD",
+    url="https://github.com/nicolasbock/gentoo-test-package",
     long_description=long_description,
     long_description_content_type="text/rst",
     use_scm_version=True,
